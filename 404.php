8a46d7544c81811e1ee01d6d21c7661e87162438
Afshin Akhgar <afshin.akhgar@gmail.com>
2016-09-05 15:21:24 +0430

    add  index and 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>error - 404</title>
+	</head>
+
+	<body>
+		<h1>page not  found 404 error</h1>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akhgar-af
+ * Date: 8/9/2016
+ * Time: 10:46 AM
+ */
+
+GLOBAL $includePath;
+GLOBAL $assetsUrl;
+$includePath = '/';
+				
+require_once './lib/router.php';
+$router = new Router();
+return $router->go(array(
+	'afshin'=>'examples/afshin.php'
+));
